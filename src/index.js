097c9415094485cb916a988cf1a9707d1bdc3750
Pedro Luiz <pedrobkluiz@hotmail.com>
2022-10-03 17:40:28 -0300

    função adicionar lista

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import cronometro from './cronometro.js'
 
 cronometro()
+
+ 
+  const selecionarTabela = document.querySelectorAll('a[href^="esquerdo"]')
+  
+ function callbackTabela(event) {
+  event.preventDefault()
+  
+  event.currentTarget.classList.toggle('ativo')
+ }
+
+  selecionarTabela.forEach((li)=> {
+    li.addEventListener('click', callbackTabela)
+  })
