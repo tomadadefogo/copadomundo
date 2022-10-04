d8b87ba2e12cd5cef1bc0bb409609952611a95df
Pedro Luiz <pedrobkluiz@hotmail.com>
2022-10-03 23:09:20 -0300

    laligafeito

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import cronometro from './cronometro.js'
 cronometro()
 
  
-  const selecionarTabela = document.querySelectorAll('a[href^="esquerdo"]')
+  const selecionarTabela = document.querySelectorAll('a[href^="premier"]')
   
  function callbackTabela(event) {
   event.preventDefault()
@@ -11,10 +11,26 @@ cronometro()
   })
   event.target.classList.toggle('ativo')
   if (event.target.classList.toggle('ativo') === false) {
-    document.querySelector('.score').classList.toggle('.ativo')
+    document.querySelector('.score').classList.toggle('ativo')
   } 
  }
 
   selecionarTabela.forEach((li)=> {
     li.addEventListener('click', callbackTabela)
+  })
+
+  const selecionarTabela2 = document.querySelectorAll('a[href^="laliga"]')
+  
+ function callbackTabela2(event) {
+  event.preventDefault()
+  selecionarTabela2.forEach((li) => {
+  })
+  event.target.classList.toggle('ativo2')
+  if (event.target.classList.toggle('ativo2') === false) {
+    document.querySelector('.score2').classList.toggle('ativo2')
+  } 
+ }
+
+  selecionarTabela2.forEach((li)=> {
+    li.addEventListener('click', callbackTabela2)
   })
